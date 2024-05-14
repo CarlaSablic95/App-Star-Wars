2efc8f7277428355f20ce40d95ab69c8adc0d607
CarlaSablic95 <carlasablic95@gmail.com>
2024-05-14 15:19:44 -0300

    add(-): Creating the films page and adding styles

---
+++ src/app/characters/page.jsx
@@ -0,0 +1,9 @@
+const Characters = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>CHARACTERS</h1>
+    </main>
+  );
+}
+
+export default Characters;
---
+++ src/app/films-details/page.jsx
@@ -0,0 +1,11 @@
+
+
+const FilmsDetails = () => {
+    return (
+        <>
+            <h1>Detalle de películas</h1>
+        </>
+    )
+}
+
+export default FilmsDetails;
