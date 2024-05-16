21c2403981f67f539ef8d2a5571753d4891dd605
CarlaSablic95 <carlasablic95@gmail.com>
2024-05-16 18:20:02 -0300

    fix(-): Fixing the styles in mobile version

--- src/app/characters/[characterId]/page.jsx
+++ src/app/characters/[characterId]/page.jsx
@@ -17,32 +17,34 @@ export default async function Character ({ params }) {
     console.log("CHARACTER: ", characterDetail)
 
     return (
-        <section className="h-full pb-44">
-            <h1 className="text-center py-5 text-3xl font-medium mb-4">Character details</h1>
-            <div className="container-fluid">
-                <div className="grid grid-cols-1">
-                    {
-                        <div className="flex justify-evenly">
-                        <div>
-                            <Image 
-                                src={ Personaje }
-                                className="w-96 drop-shadow-lg rounded-t-3xl md:rounded-3xl"
-                                alt="Character image generic"
-                            />
+        <main className="pb-24">
+            <section className="h-full pb-44">
+                <h1 className="text-center py-5 text-3xl font-medium mb-4">Character details</h1>
+                <div className="container-fluid">
+                    <div className="grid grid-cols-1">
+                        {
+                            <div className="flex flex-col sm:justify-center md:flex-row md:justify-evenly">
+                            <div className="flex justify-center px-10 md:px-0">
+                                <Image 
+                                    src={ Personaje }
+                                    className="w-96 drop-shadow-lg rounded-3xl mb-5"
+                                    alt="Character image generic"
+                                />
+                            </div>
+                            <div>
+                            <h4 className="text-lg font-semibold text-center md:text-start">Name:  <span className="font-normal">{ characterDetail.name }</span></h4>
+                            <h4 className="text-lg font-semibold text-center md:text-start">Eye color: <span className="font-normal"> { characterDetail.eye_color }</span></h4>
+                            <h4 className="text-lg font-semibold text-center md:text-start">Birthyear: <span className="font-normal"> { characterDetail.birth_year }</span></h4>
+                            <h4 className="text-lg font-semibold text-center md:text-start">Hair color: <span className="font-normal"> { characterDetail.hair_color }</span></h4>
+                            <h4 className="text-lg font-semibold text-center md:text-start">Height: <span className="font-normal"> { characterDetail.height }</span></h4>
+                            <h4 className="text-lg font-semibold text-center md:text-start">Skin color: <span className="font-normal"> { characterDetail.skin_color }</span></h4>
+                            <h4 className="text-lg font-semibold text-center md:text-start">Mass: <span className="font-normal"> { characterDetail.mass}</span></h4>
+                            </div>
                         </div>
-                        <div>
-                        <h4 className="text-lg font-semibold">Name:  <span className="font-normal">{ characterDetail.name }</span></h4>
-                        <h4 className="text-lg font-semibold">Eye color: <span className="font-normal"> { characterDetail.eye_color }</span></h4>
-                        <h4 className="text-lg font-semibold">Birthyear: <span className="font-normal"> { characterDetail.birth_year }</span></h4>
-                        <h4 className="text-lg font-semibold">Hair color: <span className="font-normal"> { characterDetail.hair_color }</span></h4>
-                        <h4 className="text-lg font-semibold">Height: <span className="font-normal"> { characterDetail.height }</span></h4>
-                        <h4 className="text-lg font-semibold">Skin color: <span className="font-normal"> { characterDetail.skin_color }</span></h4>
-                        <h4 className="text-lg font-semibold">Mass: <span className="font-normal"> { characterDetail.mass}</span></h4>
-                        </div>
+                        }
                     </div>
-                    }
                 </div>
-            </div>
-        </section>
+            </section>
+        </main>
     )
 }
--- src/app/films/[filmId]/page.jsx
+++ src/app/films/[filmId]/page.jsx
@@ -27,57 +27,59 @@ export default async function Film({ params }) {
   const filmDetail = await getFilmDetails(filmId);
 
   return (
-    <section className="h-full">
-      <h1 className="text-center py-5 text-3xl font-medium mb-4">
-        Film details
-      </h1>
-      <div className="container-fluid">
-        <div className="grid grid-cols-1">
-          <div className="flex justify-evenly">
-            <div>
-              <Image
-                src={Portada}
-                className="w-96 drop-shadow-lg rounded-t-3xl md:rounded-3xl"
-              />
-            </div>
-            <div>
-              <h4 className="text-lg font-semibold">
-                Title: <span className="font-normal">{filmDetail.title}</span>
-              </h4>
-              <h4 className="text-lg font-semibold">
-                Episode number:{" "}
-                <span className="font-normal">{filmDetail.episode_id}</span>
-              </h4>
-              <h4 className="text-lg font-semibold">
-                Director:{" "}
-                <span className="font-normal">{filmDetail.director}</span>
-              </h4>
-              <h4 className="text-lg font-semibold mb-4">Characters: </h4>
-
-              <div className="columns-4 ps-3">
-                <div>
-                  {filmDetail.characters.map((character, index) => (
-                    <div key={index} className="pe-4">
-                      <Image
-                        src={Personajes}
-                        className="w-24 drop-shadow-lg rounded-3xl"
-                        alt="Characters image generic"
-                      />
-                      <h4 className="text-lg font-semibold">
-                        <span className="font-normal">Name: </span>{" "}
-                        {character.name}
-                      </h4>
-                      <Link href="" className="text-red-500">
-                        URL
-                      </Link>
-                    </div>
-                  ))}
+    <main className="pb-24">
+        <section className="h-full">
+        <h1 className="text-center py-5 text-3xl font-medium mb-4">
+            Film details
+        </h1>
+        <div className="container-fluid">
+            <div className="grid grid-cols-1">
+            <div className="flex flex-col sm:justify-center md:flex-row md:justify-evenly">
+                <div className="flex justify-center align-start px-10 mb-5 md:mb-0">
+                <Image
+                    src={Portada}
+                    className="w-96 drop-shadow-lg rounded-3xl"
+                />
                 </div>
-              </div>
+                <div className="text-center md:text-start">
+                <h4 className="text-lg font-semibold">
+                    Title: <span className="font-normal">{filmDetail.title}</span>
+                </h4>
+                <h4 className="text-lg font-semibold">
+                    Episode number:{" "}
+                    <span className="font-normal">{filmDetail.episode_id}</span>
+                </h4>
+                <h4 className="text-lg font-semibold">
+                    Director:{" "}
+                    <span className="font-normal">{filmDetail.director}</span>
+                </h4>
+                <h4 className="text-lg font-semibold mb-4">Characters: </h4>
+
+                <div className="columns-4 px-10 md:px-0">
+                    <div className="">
+                    {filmDetail.characters.map((character, index) => (
+                        <div key={index} className="pe-4">
+                        <Image
+                            src={Personajes}
+                            className="w-24 drop-shadow-lg rounded-3xl"
+                            alt="Characters image generic"
+                        />
+                        <h4 className="text-lg font-semibold">
+                            <span className="font-normal">Name: </span>{" "}
+                            {character.name}
+                        </h4>
+                        <Link href="" className="text-red-500">
+                            URL
+                        </Link>
+                        </div>
+                    ))}
+                    </div>
+                </div>
+                </div>
             </div>
-          </div>
+            </div>
         </div>
-      </div>
-    </section>
+        </section>
+    </main>
   );
 }
